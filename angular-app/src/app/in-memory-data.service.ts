666844e4af1eaaf127c2a50bea3ed6e8fcd1bd06
Praveen <praveen.chinthala@yahoo.co.in>
2021-08-24 19:31:02 +0530

    updating products list

--- angular-app/src/app/in-memory-data.service.ts
+++ angular-app/src/app/in-memory-data.service.ts
@@ -77,20 +77,20 @@ function getDbData() {
   const products: any[] = [
     {
       id: 10,
-      name: 'Strawberries',
-      description: '16oz package of fresh organic strawberries',
-      quantity: '1',
+      name: 'iPhone 12',
+      description: 'Best iPhone yet',
+      quantity: '12',
     },
     {
       id: 20,
-      name: 'Sliced bread',
-      description: 'Loaf of fresh sliced wheat bread',
+      name: 'Samsung Curved monitor',
+      description: '35 inch wide monitor , perfect for work',
       quantity: 1,
     },
     {
       id: 30,
-      name: 'Apples',
-      description: 'Bag of 7 fresh McIntosh apples',
+      name: 'Ikea desk stand/sit adjustable',
+      description: 'Desk for standing and sitting for work',
       quantity: 1,
     },
   ];
